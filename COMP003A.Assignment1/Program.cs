4384ef355f4c95e9a1716b4a6e6ef2f68e6b7328
jojos <jojos@JOSEPH>
2024-01-20 17:24:16 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,119 @@
+﻿// this is a single-line comment
+/* this is a multi-line comment
+ * 
+ * 
+ * Author:  Joseph Sandoval
+ * Course:  COMP-003A-L01
+ * Purpose: Code for Assignment 1
+ */
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1. I love CHeese");
+            Console.WriteLine("2. I love apples");
+            Console.WriteLine("3. I love grapes");
+            Console.WriteLine("4. I love gaming");
+            Console.WriteLine("5. I love tukerys");
+            Console.WriteLine("6. I love COD");
+            Console.WriteLine("7. I love battlefield");
+            Console.WriteLine("8. I love art");
+            Console.WriteLine("9. I love crab legs");
+            Console.WriteLine("10. I love dry erase markers");
+            Console.WriteLine("11. I love notebooks");
+            Console.WriteLine("12. I love Iphones");
+            Console.WriteLine("13. I love playstation");
+            Console.WriteLine("14. I love xbox");
+            Console.WriteLine("15. I love books");
+            Console.WriteLine("16. I love netflix");
+            Console.WriteLine("17. I love english");
+            Console.WriteLine("18. I love disneyland");
+            Console.WriteLine("19. I love trees");
+            Console.WriteLine("20. I love playing guitar");
+            Console.WriteLine("21. I love playing music");
+            Console.WriteLine("22. I love snowboarding");
+            Console.WriteLine("23. I love hiking");
+            Console.WriteLine("24. I love watching sunsets");
+            Console.WriteLine("25. I love cooking");
+            Console.WriteLine("26. I love writing");
+            Console.WriteLine("27. I love spending time with my daughter");
+            Console.WriteLine("28. I love opening presents");
+            Console.WriteLine("29. I love doing math");
+            Console.WriteLine("30. The last one is a lie");
+            Console.WriteLine("31. I love staying busy");
+            Console.WriteLine("32. I love tacos");
+            Console.WriteLine("33. I love apples and bananas");
+            Console.WriteLine("34. I love exercising");
+            Console.WriteLine("35. how is your day?");
+            Console.WriteLine("36. This is repetitive");
+            Console.WriteLine("37. I guess thats the point");
+            Console.WriteLine("38. This is pretty cool, ngl");
+            Console.WriteLine("39. I love Portland Oregon");
+            Console.WriteLine("40. I love paddleboarding");
+            Console.WriteLine("41. I love bagels");
+            Console.WriteLine("42. I love disipline");
+            Console.WriteLine("43. I love billie eyelash");
+            Console.WriteLine("44. I love homework");
+            Console.WriteLine("45. I once paraglided through the Austrian Alps");
+            Console.WriteLine("46. I once bunjeed jumped off a bridge in Washington");
+            Console.WriteLine("47. I love travelling");
+            Console.WriteLine("48. I love how fast and slow time is");
+            Console.WriteLine("49. The greatest torture device created");
+            Console.WriteLine("50. I cant wait till im an engineer");
+            Console.WriteLine("51. I heard physics is kinda hard");
+            Console.WriteLine("52. I also heard the grass is greeener on the otherside");
+            Console.WriteLine("53. I am not making sense");
+            Console.WriteLine("54. I want to rule the world");
+            Console.WriteLine("55. I want to eat a ham sandwhich");
+            Console.WriteLine("56. May the force be with you");
+            Console.WriteLine("57. I have the highground now anankin");
+            Console.WriteLine("58. I see the light at the end of the tunnel");
+            Console.WriteLine("59. Semper Fidelis");
+            Console.WriteLine("60. save the trees");
+            Console.WriteLine("61. Napolean dynamite");
+            Console.WriteLine("62. im batman");
+            Console.WriteLine("63. I love Marvel");
+            Console.WriteLine("64. I love interstellar");
+            Console.WriteLine("65. I love going for drives");
+            Console.WriteLine("66. I love viewpoints");
+            Console.WriteLine("67. Adam sandler is funny");
+            Console.WriteLine("68. reality is a lie");
+            Console.WriteLine("69. I love baseball");
+            Console.WriteLine("70. I want a million dollars");
+            Console.WriteLine("71. I wish I could fly");
+            Console.WriteLine("72. I wish i knew how to ride a motorcycle");
+            Console.WriteLine("73. I wish I didnt blow my money on food");
+            Console.WriteLine("74. I wish upon a star");
+            Console.WriteLine("75. IM GOING TO RACE FOR NASCAR ONE DAY");
+            Console.WriteLine("76. Im not really a fan of candy");
+            Console.WriteLine("77. I like raves");
+            Console.WriteLine("78. Never been to a rock concert");
+            Console.WriteLine("79. sony cameras are cool");
+            Console.WriteLine("80. Boss fights are cool");
+            Console.WriteLine("81. stay composed");
+            Console.WriteLine("82. Chess is lowkey kinda hard");
+            Console.WriteLine("83. I miss the barracks culture");
+            Console.WriteLine("84. I like the ocean");
+            Console.WriteLine("85. I prefer colder weather");
+            Console.WriteLine("86. I prefer higher altitude");
+            Console.WriteLine("87. I know what the secret to life is");
+            Console.WriteLine("88. CONFIDENTIAL");
+            Console.WriteLine("89. Little Ceasers is mid");
+            Console.WriteLine("90. Indian food made in germany is better");
+            Console.WriteLine("91. cheesus christ");
+            Console.WriteLine("92. almost done");
+            Console.WriteLine("93. after this im playing ps5");
+            Console.WriteLine("94. VIVA LA REVOLUTION");
+            Console.WriteLine("95. I Want to get my pilots license one day");
+            Console.WriteLine("96. This life is pretty something");
+            Console.WriteLine("97. the game is rigged");
+            Console.WriteLine("98. game of thrones is cool");
+            Console.WriteLine("99. dolphins are evil");
+            Console.WriteLine("100. finally done");
+
+        }
+    }
+}
